942ffa341bca1d23699f66f44d83d12818430dd1
Eugeny Vlasov <eugeny.vlasov@gmail.com>
2026-02-23 09:14:12 +0300

    Add TODO IRepository comment for the future

--- LibraryManager.Application/Repositories/IRepository.cs
+++ LibraryManager.Application/Repositories/IRepository.cs
@@ -1,6 +1,9 @@
 ﻿namespace LibraryManager.Application.Repositories
 {
-    public interface IRepository<T> where T : class
+    public interface IRepository<T> where T : class //TODO - Это просто generic-класс для EfBookRepository и EfAuthorRepository. Но,
+        //как только появляются write-поддерживающие методы типа GetBookByIsbn (сейчас сделано как GetBookByIsbnQuery-класс) - сразу же нужно разделять
+        //на IBookRepository и IAuthorRepository, и в каждом реализовывать свои операции чтения-записи.
+        //Шаблонный класс это не догма, и не шаблон. Просто тут вначале это было удобнее
     {
         void Add(T entity);
         void Remove(T entity);
